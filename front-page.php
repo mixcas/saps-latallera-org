3f027eccc0797b4acd53023b4a444ba5baa94869
cas <casska@gmail.com>
2016-03-18 12:16:21 -0600

    add loop without title to front page

--- front-page.php
+++ front-page.php
@@ -5,7 +5,40 @@ get_header();
 <!-- main content -->
 
 <main id="main-content">
-  <p> HOme </p>
+  <div class="row">
+
+  <!-- main posts loop -->
+    <section id="posts" class="col s18">
+
+<?php
+if( have_posts() ) {
+  while( have_posts() ) {
+    the_post();
+?>
+
+      <article <?php post_class(); ?> id="post-<?php the_id(); ?>">
+
+        <div class="copy">
+          <?php the_content(); ?>
+        </div>
+
+      </article>
+
+<?php
+  }
+} else {
+?>
+      <article class="u-alert"><?php _e('Sorry, no posts matched your criteria :{'); ?></article>
+<?php
+} ?>
+
+  <!-- end posts -->
+    </section>
+
+    <div class="col s6">
+    <?php get_template_part('partials/main-sidebar'); ?>
+    </div>
+  </div>
 
 <!-- end main-content -->
 
