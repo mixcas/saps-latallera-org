35cadc9259b029b7c81386c8bef5e5dbf7dc5115
cas <casska@gmail.com>
2016-03-18 19:10:48 -0600

    add footer info

--- footer.php
+++ footer.php
@@ -1,4 +1,8 @@
     <footer id="footer">
+      <p><strong>SAPS:</strong> <a href="mailto:difusion@saps-latallera.org">difusion@saps-latallera.org</a> | +52 8647 5340 | <a href="https://www.facebook.com/saladeartepublicosiqueiros" target="_blank">Facebook</a> | <a href="https://twitter.com/SAPSiqueiros" target="_blank">Twitter</a></p>
+      <p><strong>La Tallera:</strong> <a href="mailto:difusion-sapslatallera@inba.gob.mx">difusion-latallera@inba.gob.mx</a> | +777 160 11 90 | <a href="https://www.facebook.com/la.tallera" target="_blank">Facebook</a> | <a href="https://twitter.com/LaTallera" target="_blank">Twitter</a></p>
+      <p>PROYECTO SIQUEIROS: Sala de Arte Público Siqueiros - La Tallera, pertenece a la red de museos de la SEP, INBA y CONACULTA.</p>
+      <p>Copyright © <?php echo date("Y"); ?> Proyecto Siqueiros</p>
     </footer>
 
   </section>
