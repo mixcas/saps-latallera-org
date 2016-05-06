d6966f7881e4c39041be1e239fffbfd7b4414d25
cas <casska@gmail.com>
2016-05-06 15:53:49 -0500

    add lang copy to pagination

--- partials/pagination.php
+++ partials/pagination.php
@@ -4,8 +4,8 @@ if( get_next_posts_link() || get_previous_posts_link() ) {
   <!-- post pagination -->
   <nav id="pagination">
 <?php
-$previous = get_previous_posts_link('Newer');
-$next = get_next_posts_link('Older');
+$previous = get_previous_posts_link( __( '[:es]Recientes[:en]Newer[:]' ));
+$next = get_next_posts_link( __('[:es]Anteriores[:en]Older[:]') );
 if ($previous) {
   echo $previous;
 }
