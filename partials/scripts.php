ce926d673e19c191cd6f454e5ae89d3cd068dc28
cas <casska@gmail.com>
2016-05-09 14:40:41 -0500

    add google analytics

--- partials/scripts.php
+++ partials/scripts.php
@@ -2,4 +2,14 @@
   <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
   <script>window.jQuery || document.write('<script src="<?php bloginfo('stylesheet_directory'); ?>/js/vendor/jquery-2.1.1.min.js"><\/script>')</script>
   <?php wp_footer(); ?>
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-77003156-1', 'auto');
+    ga('send', 'pageview');
+
+  </script>
 </section>
